46aaf2906b99a8f2972660dc650382c3ed568b0d
Leonardo Freire Russi <leonardofreire.russi@icloud.com>
2022-02-06 22:27:20 -0300

    :wrench: setting up some webpack plugins and loaders

--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,6 @@
-import React from "react";
-import { App } from './App'
+import { render } from "react-dom";
+import { App } from './App';
+
+const ROOT = document.getElementById('root');
+
+render(<App />, ROOT);
